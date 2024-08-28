5099cb2c3f8388164b708c33aedb1826f9a7d4d3
hieuho <hieuhd1003@gmail.com>
2024-08-28 19:33:03 +0700

    test

--- index.js
+++ index.js
@@ -2,50 +2,69 @@ const fs = require('fs');
 const https = require('https');
 const express = require('express');
 const socketIO = require('socket.io');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
+// SSL certificate options
 const options = {
     key: fs.readFileSync('./server.key'),
     cert: fs.readFileSync('./server.crt')
 };
 
+// Create Express application
 const app = express();
-const server = https.createServer(options, app);
-const io = socketIO(server);
-
 app.use(express.static('public'));
 
-let users = {}; // { userId: { type: 'admin'|'user', socketId: socket.id } }
+if (cluster.isMaster) {
+    // Fork workers for each CPU core
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
 
-io.on('connection', (socket) => {
-    console.log('New user connected:', socket.id);
-
-    socket.on('join-room', (type) => {
-        users[socket.id] = { type, socketId: socket.id };
-        
-        // Find the other user
-        const otherUser = Object.keys(users).find(id => id !== socket.id);
-
-        if (otherUser) {
-            // Notify the other user about the new connection
-            io.to(otherUser).emit('user-connected', socket.id);
-            io.to(socket.id).emit('user-connected', otherUser); // Notify the new user
-        }
+    // Restart workers if they die
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died. Restarting...`);
+        cluster.fork();
     });
+} else {
+    // Create HTTPS server with Express
+    const server = https.createServer(options, app);
+    const io = socketIO(server);
 
-    socket.on('signal', (data) => {
-        io.to(data.to).emit('signal', {
-            signal: data.signal,
-            from: socket.id
+    let users = {}; // { userId: { type: 'admin'|'user', socketId: socket.id } }
+
+    io.on('connection', (socket) => {
+        console.log('New user connected:', socket.id);
+
+        socket.on('join-room', (type) => {
+            users[socket.id] = { type, socketId: socket.id };
+
+            // Find the other user
+            const otherUser = Object.keys(users).find(id => id !== socket.id);
+
+            if (otherUser) {
+                // Notify the other user about the new connection
+                io.to(otherUser).emit('user-connected', socket.id);
+                io.to(socket.id).emit('user-connected', otherUser); // Notify the new user
+            }
+        });
+
+        socket.on('signal', (data) => {
+            io.to(data.to).emit('signal', {
+                signal: data.signal,
+                from: socket.id
+            });
+        });
+
+        socket.on('disconnect', () => {
+            console.log('User disconnected:', socket.id);
+            delete users[socket.id];
+            socket.broadcast.emit('user-disconnected', socket.id);
         });
     });
 
-    socket.on('disconnect', () => {
-        console.log('User disconnected:', socket.id);
-        delete users[socket.id];
-        socket.broadcast.emit('user-disconnected', socket.id);
+    // Start server on port 3000
+    server.listen(3000, () => {
+        console.log('Server is running on port 3000');
     });
-});
-
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+}
